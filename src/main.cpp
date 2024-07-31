2e6879d1f0782ff4f62286213c5c941e983305db
Okkamov <okkamov@gmail.com>
2024-07-31 15:54:48 +0600

    Внесены изменения для тестирования сборки

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,13 @@
 #include "../include/list.hpp"
 #include <fmt/core.h>
 #include <fmt/format.h>
+#include <iostream>
 #include <zlib.h>
-//#include <iostream>
 
-int main(int, char**) {
+int main(int, char **) {
     fmt::print("Hello for GitHub Actions!\n\n\n");
-    fmt::print("FMT version is {}\n\n",
-               "ZLIB version is {}\n", 
-               FMT_VERSION, ZLIB_VERSION);
+    fmt::print("FMT version is {}\n\n", "ZLIB version is {}\n", FMT_VERSION,
+               ZLIB_VERSION);
 
     const size_t count = 6;
 
@@ -19,11 +18,11 @@ int main(int, char**) {
     }
 
     for (size_t i = 0; i < count * 2; ++i) {
-        fmt::print(fmt::format("Pushed into list {}\n",list.front()));
-        //std::cout << list.front() << ' ';
+        fmt::print(fmt::format("Pushed into list {}\n", list.front()));
+        // std::cout << list.front() << ' ';
         list.pop_front();
     }
-//    std::cout << std::endl;
+    std::cout << std::endl;
 
     return 0;
 }
