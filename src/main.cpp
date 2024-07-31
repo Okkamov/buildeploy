c4525831081aa1f3db7c9341f2347352b7f62ca0
Okkamov <okkamov@gmail.com>
2024-07-31 22:36:14 +0600

    fix bugs

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include "../include/list.hpp"
 #include <fmt/core.h>
-#include <fmt/format.h>
 #include <iostream>
 #include <zlib.h>
 
 int main(int, char **) {
     fmt::print("Hello for GitHub Actions!\n\n\n");
-    fmt::print("FMT version is {}\n\n", "ZLIB version is {}\n", FMT_VERSION,
-               ZLIB_VERSION);
+    fmt::print("FMT version is {}\n", FMT_VERSION);
+    fmt::print("ZLIB version is {}\n\n", ZLIB_VERSION);
 
+    std::cout << "FMT version is [" << FMT_VERSION << "]" << std::endl;
+    std::cout << "ZLIB version is [" << ZLIB_VERSION << "]\n" << std::endl;
     const size_t count = 6;
 
     otus::List<size_t> list;
